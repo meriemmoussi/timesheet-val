411c639d985712f670492e0b9528e0af15714e6a
meriemmoussi <meriem.moussi@esprit.tn>
2021-02-25 23:08:40 +0100

    meriemcommit

--- src/main/java/tn/esprit/spring/repository/DepartementRepository.java
+++ src/main/java/tn/esprit/spring/repository/DepartementRepository.java
@@ -1,11 +1,28 @@
 package tn.esprit.spring.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import tn.esprit.spring.entities.Departement;
+
+
 
 @Repository
 public interface DepartementRepository extends CrudRepository<Departement, Integer>{
 
+   
+    
+
+    
+    
+
+	
+    		
+  
 }
--- src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
@@ -2,27 +2,52 @@ package tn.esprit.spring.services;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import tn.esprit.spring.entities.Departement;
-import tn.esprit.spring.entities.Employe;
+
 import tn.esprit.spring.repository.DepartementRepository;
 
 @Service
 public class DepartementServiceImpl implements IDepartementService {
-
+	
 	@Autowired
 	DepartementRepository deptRepository;
-
+	private static final Logger l = LogManager.getLogger(DepartementServiceImpl.class);
 	public List<Departement> getAllDepartements() {
-		return (List<Departement>) deptRepository.findAll();
+		List<Departement> depNames= (List<Departement>) deptRepository.findAll();
+		l.info("*******La liste des départements est : ");
+		
+		if (depNames.isEmpty()) {
+			l.error("vide********");
+		} else {
+			l.info(depNames + "*********");
+		}
+		return depNames;
 	}
 
 	@Override
 	public Departement addOrUpdateDep(Departement d) {
 		Departement dept = deptRepository.save(d);
+		l.info("**********Le departement  est ajouté/mis a jour avec succés " + dept);
 		return dept;
+	}
+
+	
+
+	@Override
+	public Departement getDepartementById(int depId) {
+		if(depId<0) {
+			l.error("*******Error,Invalide identifier");
+		}
+		else {
+			l.info("*****Le nom du departement est " + deptRepository.findById(depId).get().getName()+ "********");
+
+		}
+		return deptRepository.findById(depId).get();
 	}
 	
 	
