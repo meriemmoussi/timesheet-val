17012ee61677aaacb5f32574e96e1403f56ba5b2
MariemZ18 <mariam.zoghlami.103@gmail.com>
2021-02-26 00:31:14 +0100

    Entreprise commit

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -3,6 +3,8 @@ package tn.esprit.spring.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,13 +22,17 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	@Autowired
 	DepartementRepository deptRepoistory;
 	
+	private static final Logger logger = LogManager.getLogger(EntrepriseServiceImpl.class);
+	
 	public int ajouterEntreprise(Entreprise entreprise) {
-		entrepriseRepoistory.save(entreprise);
+		Entreprise e = entrepriseRepoistory.save(entreprise);
+		logger.info("*************Entreprise ajoutée avec succès " + entreprise);
 		return entreprise.getId();
 	}
 
 	public int ajouterDepartement(Departement dep) {
 		deptRepoistory.save(dep);
+		logger.info("*************Département ajouté avec succès " + dep);
 		return dep.getId();
 	}
 	
@@ -47,26 +53,46 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
 		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
 		List<String> depNames = new ArrayList<>();
-		for(Departement dep : entrepriseManagedEntity.getDepartements()){
+		logger.info("*************Départements : ");
+		for (Departement dep : entrepriseManagedEntity.getDepartements()) {
 			depNames.add(dep.getName());
 		}
-		
+		if (depNames.isEmpty()) {
+			logger.error("L'entreprise n'a pas de départements*************");
+		}
+
 		return depNames;
 	}
 
 	@Transactional
 	public void deleteEntrepriseById(int entrepriseId) {
-		entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());	
+		if (entrepriseId < 0) {
+			logger.error("*************Error,Identifiant invalide*************");
+		} else {
+			logger.error("L'entreprise avec identifinat " + entrepriseId + "supprimée avec succès*************");
+		}
+		entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());
 	}
 
 	@Transactional
 	public void deleteDepartementById(int depId) {
-		deptRepoistory.delete(deptRepoistory.findById(depId).get());	
+		if (depId < 0) {
+			logger.debug("*************Error,Identifiant invalide*************");
+		} else {
+			logger.error("Département avec l'identifinat " + depId + "supprimé avec succès*************");
+		}
+		deptRepoistory.delete(deptRepoistory.findById(depId).get());
 	}
 
-
 	public Entreprise getEntrepriseById(int entrepriseId) {
-		return entrepriseRepoistory.findById(entrepriseId).get();	
+		if(entrepriseId<0) {
+			logger.error("*************Error,identifiant invalide");
+		}
+		else {
+			logger.info("**************Le nom du l'entrprise est " + entrepriseRepoistory.findById(entrepriseId).get().getName()+ "*************");
+
+		}
+		return entrepriseRepoistory.findById(entrepriseId).get();
 	}
 
 }
